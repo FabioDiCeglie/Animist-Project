e93414e432febc7928fc2ebd388b0dd93382694c
FabioDiCeglie <dcfabio96@gmail.com>
2022-05-05 15:17:47 +0200

    pagination mangas

--- pages/mangas/index.tsx
+++ pages/mangas/index.tsx
@@ -1,5 +1,16 @@
-import { Fragment } from "react";
-import { Container, Grid, Typography } from "@mui/material";
+import { Fragment, useState } from "react";
+import {
+  Container,
+  Grid,
+  Typography,
+  Card,
+  CardContent,
+  CardActionArea,
+  CardActions,
+  CardMedia,
+  Button,
+  Box,
+} from "@mui/material";
 import PaginationComponent from "../../components/Pagination";
 
 interface data {
@@ -10,6 +21,8 @@ interface data {
       image_url: string;
     };
   };
+  url: string;
+  rank: number;
 }
 
 export async function getServerSideProps() {
@@ -20,24 +33,63 @@ export async function getServerSideProps() {
 }
 
 const Mangas = ({ mangas }: any) => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [mangasPerPage, setMangas] = useState<number>(4);
   if (!mangas) {
     return <>Loading</>;
   }
 
+  console.log(mangas);
+  //PageNumbers
+  const pageNumbers = mangas.data.length / mangasPerPage;
+
+  //Get current games
+  const indexOfLastMangas = currentPage * mangasPerPage;
+  const indexOfFirstPost = indexOfLastMangas - mangasPerPage;
+  const currentMangas = mangas.data.slice(indexOfFirstPost, indexOfLastMangas);
+
+  //Change page
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   return (
-    <Container>
+    <Container sx={{ mt: 5 }}>
+      <Typography variant="h4">Famous mangas</Typography>
+      <Box sx={{ mb: 5 }}>
+        <PaginationComponent paginate={paginate} pageNumbers={pageNumbers} />
+      </Box>
       <Grid container spacing={2}>
-        {mangas.data.map((manga: data) => (
+        {currentMangas.map((manga: data) => (
           <Fragment key={manga.id}>
             <Grid item xs={8} lg={6}>
-              <Typography variant="h4">{manga.title}</Typography>
-
-              <img
-                src={manga.images.jpg.image_url}
-                alt={manga.title}
-                width={400}
-                height={400}
-              />
+              <Card sx={{ maxWidth: 400 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={manga.images.jpg.image_url}
+                    alt={manga.title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {manga.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Rank: {manga.rank}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    href={manga.url}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Check the manga!
+                  </Button>
+                </CardActions>
+              </Card>
             </Grid>
           </Fragment>
         ))}
